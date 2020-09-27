e36f55ede2c3db780eca5616e5f47eac9b68592e
ronggurmwp <ronggur.mwp@gmail.com>
2020-09-27 15:32:03 +0700

    fungsi Determinan dengan metorde kofaktor

--- src/com/Algeo/Determinan.java
+++ src/com/Algeo/Determinan.java
@@ -1,7 +1,47 @@
 package com.Algeo;
 
 public class Determinan {
-    public void kofaktor() {
-        System.out.println("Hello World!!!");
+    public double kofaktor(double[][] MatriksInput) {
+        if (MatriksInput[0].length != MatriksInput.length){
+            return -999 ;//error karena bukan matriks persegi
+        } else if(MatriksInput.length == 1){
+            return MatriksInput[0][0];//Basis dari rekursi
+        } else{//menghitung matriks [baris][kolom]
+            double det = 0;
+            double[][]  minor = new double[MatriksInput.length-1][MatriksInput.length-1];
+            //k adalah kolom dari MatriksInput, selalu ambil baris 0
+            //i adalah  baris MatriksInput
+            //j adalah kolom MatriksInput
+            //a adalah baris minor
+            //b adalah kolom minor
+            int k;
+            for (k = 0;k < MatriksInput.length;k++){
+                int i = 1;
+                int a = 0;
+                while(i < MatriksInput.length){
+                    int j = 0;
+                    int b = 0;
+                    while(j < MatriksInput.length){
+                        if (k != j){
+                            minor[a][b] = MatriksInput[i][j];
+                            b++;
+                        }
+                        j++;
+                    }
+                    a++;
+                    i++;
+                }
+                if(k%2 == 1){
+                    det -= MatriksInput[0][k]*this.kofaktor(minor);
+                }else {
+                    det += MatriksInput[0][k]*this.kofaktor(minor);
+                }
+            }
+            return det;
+        }
+
+    }
+    public double OBE(double[][] MatriksInput){
+        return this.kofaktor(MatriksInput);
     }
 }
--- src/com/Algeo/Main.java
+++ src/com/Algeo/Main.java
@@ -1,26 +1,29 @@
 package com.Algeo;
 import java.util.Scanner;
-
-
+import com.Algeo.Determinan   ;
+//import package Determinan;
 public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Initilizing main");
+        Determinan determinan = new Determinan();
 	    double[][] matriks;
 	    matriks = bacaMatriks();
         tulisMatriks(matriks);
+        System.out.print("Determinan Matriks dengan kofaktor = ");
+        System.out.println(determinan.kofaktor(matriks));
     }
 
     public static double[][] bacaMatriks() {
         Scanner myObj = new Scanner(System.in);
-        System.out.print("Enter Column = ");
-        int column = myObj.nextInt();  // Read user input
-        System.out.print("Enter Row = ");
-        int row = myObj.nextInt();  // Read user input
-        double matriks[][] = new double[row][column];
-        for(int i = 0; i< row;i++){
-            for(int j = 0;j<column;j++){
-                System.out.print(String.format("Enter Matriks[%d][%d] = ", i, j));
+        System.out.print("Masukan Jumlah Kolom : ");
+        int kolom = myObj.nextInt();  // Read user input
+        System.out.print("Masukan Jumlah Baris : ");
+        int baris = myObj.nextInt();  // Read user input
+        double matriks[][] = new double[baris][kolom];
+        for(int i = 0; i< baris;i++){
+            for(int j = 0;j<kolom;j++){
+                System.out.print(String.format("Masukan Matriks[%d][%d] = ", i, j));
                 matriks[i][j] = myObj.nextDouble();
             }
         }
@@ -30,7 +33,7 @@ public class Main {
     public static void tulisMatriks(double[][] matriks){
         for (int i = 0; i < matriks.length;i++){
             for (int j = 0; j < matriks[i].length;j++){
-                System.out.printf("%f ",matriks[i][j]);
+                System.out.printf("%.2f ",matriks[i][j]);
             }
             System.out.println();
         }
