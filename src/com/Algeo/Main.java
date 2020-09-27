dfd2f61dbfcd987ad7d302d6c26b3e257b28f632
ronggurmwp <ronggur.mwp@gmail.com>
2020-09-27 13:48:37 +0700

    tulisMatriks

--- src/com/Algeo/Main.java
+++ src/com/Algeo/Main.java
@@ -6,21 +6,33 @@ public class Main {
 
     public static void main(String[] args) {
 	    System.out.println("Initilizing main");
-	    bacaMatriks();
+	    double[][] matriks;
+	    matriks = bacaMatriks();
+        tulisMatriks(matriks);
     }
 
-    public static void bacaMatriks() {
+    public static double[][] bacaMatriks() {
         Scanner myObj = new Scanner(System.in);
         System.out.print("Enter Column = ");
         int column = myObj.nextInt();  // Read user input
         System.out.print("Enter Row = ");
         int row = myObj.nextInt();  // Read user input
-        int a[][] = new int[row][column];
+        double matriks[][] = new double[row][column];
         for(int i = 0; i< row;i++){
             for(int j = 0;j<column;j++){
                 System.out.print(String.format("Enter Matriks[%d][%d] = ", i, j));
-                a[i][j] = myObj.nextInt();
+                matriks[i][j] = myObj.nextDouble();
             }
+        }
+        myObj.close();
+        return matriks;
+    }
+    public static void tulisMatriks(double[][] matriks){
+        for (int i = 0; i < matriks.length;i++){
+            for (int j = 0; j < matriks[i].length;j++){
+                System.out.printf("%f ",matriks[i][j]);
+            }
+            System.out.println();
         }
     }
 }
